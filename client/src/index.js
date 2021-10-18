5fc8f615d7b72c70f7cb5e1a659a29572097178f
Aseer KT <aseermiqdad@gmail.com>
2021-10-18 22:15:54 +0530

    chore: remove web vitals

--- client/src/index.js
+++ client/src/index.js
@@ -4,25 +4,23 @@ import './index.css';
 import App from './App';
 import { createUploadLink } from 'apollo-upload-client';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import reportWebVitals from './reportWebVitals';
 
+// `uploadLink` is a terminating link
 const uploadLink = createUploadLink({
   uri: 'http://localhost:5000/graphql',
 });
 
+// `uploadLink` is used instead of httpLink
 const client = new ApolloClient({
   link: uploadLink,
   cache: new InMemoryCache(),
 });
 
+// client instance is provided to the entire react app using ApolloProvider
+// Now we can use apollo hooks (useQuery, useMutation, ...) in child components
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
